b6507ea03d66dec9f7a6bfd6db625749caf2240a
Syed Zayyan <masudzayyan@gmail.com>
2021-06-27 20:22:22 +0100

    added Image component and broke stuff so fixin 2

--- components/Blog/BlogDetail.js
+++ components/Blog/BlogDetail.js
@@ -1,56 +1,23 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import timeformatter from "../../timeformatter"
-import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import { BLOCKS } from "@contentful/rich-text-types";
 import Image from "next/image";
 
 const renderOptions = {
-    renderNode: {
-      [INLINES.EMBEDDED_ENTRY]: (node, children) => {
-        // target the contentType of the EMBEDDED_ENTRY to display as you need
-        if (node.data.target.sys.contentType.sys.id === "blogPost") {
-          return (
-            <a href={`/blog/${node.data.target.fields.slug}`}>        
-            </a>
-          );
-        }
-      },
-      [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-        // target the contentType of the EMBEDDED_ENTRY to display as you need
-        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
-          return (
-            <pre>
-              <code>{node.data.target.fields.code}</code>
-            </pre>
-          );
-        }
-  
-        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
-          return (
-            <iframe
-              src={node.data.target.fields.embedUrl}
-              height="100%"
-              width="100%"
-              frameBorder="0"
-              scrolling="no"
-              title={node.data.target.fields.title}
-              allowFullScreen={true}
-            />
-          );
-        }
-      },
-  
-      [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-        // render the EMBEDDED_ASSET as you need
-        return (
-            <img
-            width = "60%"
-              src={`https://${node.data.target.fields.file.url}`}
-              alt={node.data.target.fields.description}
-            />
-        );
-      },
-    },
-  };
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+      <div style = {{position:"relative", width : "60vw", height : "60vh", margin : "0 auto"}}>
+      <Image
+        objectFit = "contain"
+        layout = "fill"
+        src={"https:" +  node.data?.target?.fields?.file?.url}
+        alt={node.data?.target?.fields?.title}
+      />
+      </div>
+    ),
+  },
+};
+
 export default function BlogDetail({ data }) {
     return (
         <div>
